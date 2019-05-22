064435ca5cf77f9952193db0224ba87e4debde9c
Rosh <rosh_408@hotmail.com>
2019-05-22 16:03:19 -0700

    Change html elements within the Inhabitent Journal Section // Add get_posts method in front page journal section

--- themes/redstarter/front-page.php
+++ themes/redstarter/front-page.php
@@ -4,7 +4,11 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
-		<section class="home-hero"></section>
+		<section class="header-front-page">
+			<div class="home-hero">
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/full-logo.svg">
+			</div>
+		</section>
 		<section class="front-page-products"></section>
 
 		<section class="inhabitent-journal">
@@ -14,17 +18,17 @@ get_header(); ?>
 			$args = array('post_type' => 'post', 'posts_per_page' => 3);
 			$journal_posts = get_posts($args); // returns an array of posts
 			?>
-			<div class="journal-post-content">
+			<ul class="journal-post-content">
 				<?php foreach ($journal_posts as $post) : setup_postdata($post); ?>
-					<div class="journal-post-item">
-						<a href="<?php echo get_the_permalink(); ?>"><img src="<?php the_post_thumbnail('large'); ?></a>
-						    	<h3><?php the_title(); ?></h3>
-							<p> <?php echo get_the_date(); ?> | <?php echo get_comments_number(); ?> Comments </p>
-							<a class="read-more" href="<?php echo get_the_permalink(); ?> "> Read Entry</a>
-				    	</div>
+					<li class="journal-post-item">
+						<a class="post-thumbnail" href="<?php echo get_the_permalink(); ?>"><img src="<?php the_post_thumbnail('large'); ?></a>
+						    			<h3><?php the_title(); ?></h3>
+									<p> <?php echo get_the_date(); ?> | <?php echo get_comments_number(); ?> Comments </p>
+									<a class=" read-more" href="<?php echo get_the_permalink(); ?> "> Read Entry</a>
+					</li>
 				<?php endforeach;
 			wp_reset_postdata(); ?>
-			</div>
+			</ul>
 		</section>
 
 		<section class="latest-adventures">
