682f9f2980a340597b0021e43b0571bb28d99813
Rosh <rosh_408@hotmail.com>
2019-05-23 16:21:34 -0700

    Re-arrange .site-header and main navigation on banner

--- themes/redstarter/front-page.php
+++ themes/redstarter/front-page.php
@@ -4,11 +4,10 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
-		<section class="header-front-page">
-			<div class="home-hero">
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logos/full-logo.svg">
-			</div>
-		</section>
+
+		<div class="home-hero">
+			<img class="home-hero-logo" src="<?php echo get_template_directory_uri() . '/assets/images/logos/full-logo.svg' ?>">
+		</div>
 
 		<!-- Product Terms and Icons-->
 		<section class="front-page-products">
@@ -21,32 +20,27 @@ get_header(); ?>
 				)
 			);
 			?>
-
 			<ul class="product-terms">
 				<?php
 				foreach ($terms as $term) :
 					?>
-
 					<li class="product-term">
-						<img src="<?php echo get_template_directory_uri() . 
-						'/assets/images/product-type-icons/' . 
-						$term->slug . '.svg' 
-						?>" />
+						<img src="<?php echo get_template_directory_uri() .
+										'/assets/images/product-type-icons/' .
+										$term->slug . '.svg'
+									?>" />
 						<p><?php echo $term->description; ?></p>
 						<p>
-							<a href="<?php echo get_term_link($term);?>">
-							<?php echo $term->name; ?> Stuff
-						</a>
-					</p>
+							<a href="<?php echo get_term_link($term); ?>">
+								<?php echo $term->name; ?> Stuff
+							</a>
+						</p>
 					</li>
 				<?php
 			endforeach;
 			?>
 			</ul> <!-- end of .product-terms-->
 		</section>
-
-
-
 
 		<!-- Journal Section -->
 		<section class="inhabitent-journal">
@@ -56,17 +50,19 @@ get_header(); ?>
 			$args = array('post_type' => 'post', 'posts_per_page' => 3);
 			$journal_posts = get_posts($args); // returns an array of posts
 			?>
+			<div class="journal-container">
 			<ul class="journal-post-content">
 				<?php foreach ($journal_posts as $post) : setup_postdata($post); ?>
 					<li class="journal-post-item">
 						<a class="post-thumbnail" href="<?php echo get_the_permalink(); ?>"><img src="<?php the_post_thumbnail('large'); ?></a>
-						    										<h3><?php the_title(); ?></h3>
-																<p> <?php echo get_the_date(); ?> | <?php echo get_comments_number(); ?> Comments </p>
-																<a class=" read-more" href="<?php echo get_the_permalink(); ?> "> Read Entry</a>
+						    														<h3><?php the_title(); ?></h3>
+																				<p> <?php echo get_the_date(); ?> | <?php echo get_comments_number(); ?> Comments </p>
+																				<a class=" read-more" href="<?php echo get_the_permalink(); ?> "> Read Entry</a>
 					</li>
 				<?php endforeach;
 			wp_reset_postdata(); ?>
 			</ul>
+		</div>
 		</section>
 
 		<section class="latest-adventures">
--- themes/redstarter/header.php
+++ themes/redstarter/header.php
@@ -25,20 +25,22 @@
 		<header id="masthead" class="site-header" role="banner">
 			<div class="site-branding">
 				<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
-				<p class="site-description"><?php bloginfo('description'); ?></p>
 			</div><!-- .site-branding -->
 
 			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<div class="menu-bar">
-					<a href="<?php echo get_the_permalink(); ?>"><img src="<?php echo get_template_directory_uri() . '/assets/images/logos/inhabitent-logo-tent-white.svg'?>"></a>
+				<div class="tent-container">
+					<a class="front-page-tent" href="<?php echo get_home_url() ?>"><img src="<?php echo get_template_directory_uri() . '/assets/images/logos/inhabitent-logo-tent-white.svg' ?>"></a>
+				</div>
+				<div class="right-side-navigation">
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html('Primary Menu'); ?></button>
 					<?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu')); ?>
 					<div class="header-search">
 						<?php get_search_form(); ?>
 					</div>
-
 				</div>
-			</nav><!-- #site-navigation -->
+			</nav>
+			<!-- #site-navigation -->
+
 		</header><!-- #masthead -->
 
 		<div id="content" class="site-content">
