e3e01e045aeef137f75028c1b02f0393f652af2b
Rosh <rosh_408@hotmail.com>
2019-05-23 22:58:52 -0700

    Separate widget area to the right and content area to the left

--- themes/redstarter/front-page.php
+++ themes/redstarter/front-page.php
@@ -51,23 +51,27 @@ get_header(); ?>
 			$journal_posts = get_posts($args); // returns an array of posts
 			?>
 			<div class="journal-container">
-			<ul class="journal-post-content">
-				<?php foreach ($journal_posts as $post) : setup_postdata($post); ?>
-					<li class="journal-post-item">
-						<a class="post-thumbnail" href="<?php echo get_the_permalink(); ?>"><img src="<?php the_post_thumbnail('large'); ?></a>
-						    														<h3><?php the_title(); ?></h3>
-																				<p> <?php echo get_the_date(); ?> | <?php echo get_comments_number(); ?> Comments </p>
-																				<a class=" read-more" href="<?php echo get_the_permalink(); ?> "> Read Entry</a>
-					</li>
-				<?php endforeach;
-			wp_reset_postdata(); ?>
+				<ul class="journal-post-content">
+					<?php foreach ($journal_posts as $post) : setup_postdata($post); ?>
+						<li class="journal-post-item">
+							<a class="post-thumbnail" href="<?php echo get_the_permalink(); ?>">
+								<img src="<?php the_post_thumbnail('large'); ?>
+						    	</a>
+						    <div class=" journal-post-text">
+								<h3><?php the_title(); ?></h3>
+								<p> <?php echo get_the_date(); ?> | <?php echo get_comments_number(); ?> Comments </p>
+								<a class=" read-more" href="<?php echo get_the_permalink(); ?> "> Read Entry</a>
+							</div>
+						</li>
+			<?php endforeach;
+		wp_reset_postdata(); ?>
 			</ul>
-		</div>
-		</section>
+</div>
+</section>
 
-		<section class="latest-adventures">
-		</section>
-	</main><!-- #main -->
+<section class="latest-adventures">
+</section>
+</main><!-- #main -->
 </div><!-- #primary -->
 
 <?php get_footer(); ?>
