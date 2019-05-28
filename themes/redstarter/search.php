9ed816410d9c77b2c62e9e343da6b7c3ce08c55b
Rosh <rosh_408@hotmail.com>
2019-05-27 23:05:59 -0700

    Add styling to contents section of search results page

--- themes/redstarter/search.php
+++ themes/redstarter/search.php
@@ -7,19 +7,34 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<section id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+		<?php if (have_posts()) : ?>
 
 			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html( 'Search Results for: %s' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+				<h1 class="page-title"><?php printf(esc_html('Search Results for: %s'), '<span>' . get_search_query() . '</span>'); ?></h1>
 			</header><!-- .page-header -->
 
 			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php while (have_posts()) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'search' ); ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<header class="entry-header">
+						<?php the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
+
+						<?php if ('post' === get_post_type()) : ?>
+							<div class="entry-meta">
+								<?php red_starter_posted_on(); ?> / <?php comments_number('0 Comments', '1 Comment', '% Comments'); ?> / <?php red_starter_posted_by(); ?>
+							</div><!-- .entry-meta -->
+						<?php endif; ?>
+					</header><!-- .entry-header -->
+
+					<div class="entry-summary">
+						<?php the_excerpt(); ?>
+						<p class="read-more"><a href="<?php echo get_the_permalink(); ?>">Read More &nbsp </a></p>
+					</div><!-- .entry-summary -->
+				</article><!-- #post-## -->
 
 			<?php endwhile; ?>
 
@@ -27,12 +42,12 @@ get_header(); ?>
 
 		<?php else : ?>
 
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+			<?php get_template_part('template-parts/content', 'none'); ?>
 
 		<?php endif; ?>
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
+	</main><!-- #main -->
+</section><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
