76ba931be5ae5cee30d1b47002cc0b525bb5d185
Rosh <rosh_408@hotmail.com>
2019-05-28 22:33:15 -0700

    Add Latest Adventures by hardcoding html content

--- themes/redstarter/searchform.php
+++ themes/redstarter/searchform.php
@@ -1,12 +1,11 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url('/'); ?>">
 
-	<label>
-		<input type="search" id="search-label" class="search-field" placeholder="Type and hit enter..." value="<?php echo esc_attr(get_search_query()); ?>" name="s" title="Search for:" />
-	</label>
-
 	<span class="icon-search" aria-hidden="true">
 		<i class="fa fa-search"></i>
 	</span>
+	<label>
+		<input type="search" id="search-label" class="search-field" placeholder="Type and hit enter..." value="<?php echo esc_attr(get_search_query()); ?>" name="s" title="Search for:" />
+	</label>
 	<span class="screen-reader-text"><?php echo esc_html('Search'); ?></span>
 
 
