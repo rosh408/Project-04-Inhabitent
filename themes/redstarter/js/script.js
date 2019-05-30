6e46f578cf43f1dc44e3c2c2f2e338e49d9dce6d
Rosh <rosh_408@hotmail.com>
2019-05-30 11:19:36 -0700

    Add function doc ready to script.js

--- themes/redstarter/js/script.js
+++ themes/redstarter/js/script.js
@@ -1,13 +1,15 @@
 (function($) {
-  $('.icon-search').on('click', function(event) {
-    event.preventDefault();
-    // $('.search-field').toggle('slow');
-    $('#search-label').toggleClass('after-click');
-    $('#search-label').focus();
-  });
-  $('#search-label').on('blur', function(event) {
-    event.preventDefault();
-    
-    $('#search-label').removeClass('after-click');
+  $(function() {
+    $('.icon-search').on('click', function(event) {
+      event.preventDefault();
+      // $('.search-field').toggle('slow');
+      $('#search-label').toggleClass('after-click');
+      $('#search-label').focus();
+    });
+    $('#search-label').on('blur', function(event) {
+      event.preventDefault();
+
+      $('#search-label').removeClass('after-click');
+    });
   });
 })(jQuery);
